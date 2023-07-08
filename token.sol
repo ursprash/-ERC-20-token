9e86eefd5f9bb3d6ff4d83825dacbe4da92c2502
PRASHANT TIWARI <111697531+ursprash@users.noreply.github.com>
2023-07-08 12:38:58 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract DegenToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Mint(address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    constructor() {
+        name = "Degen Gaming Token";
+        symbol = "DGN";
+        decimals = 18;
+        totalSupply = 0;
+        owner = msg.sender;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        require(amount <= balances[sender], "Insufficient balance.");
+        require(amount <= allowances[sender][msg.sender], "Insufficient allowance.");
+
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        allowances[sender][msg.sender] -= amount;
+
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        totalSupply += amount;
+        balances[to] += amount;
+
+        emit Mint(to, amount);
+        emit Transfer(address(0), to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
+
+        emit Burn(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
